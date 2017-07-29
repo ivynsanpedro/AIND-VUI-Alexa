dfaf3bf74a56a9c13e9c5562c7787b72e83afa72
Ivyn San Pedro <isanpedro@gmail.com>
2017-07-29 14:21:35 -0400

    Add more AI history facts.

--- src/facts.js
+++ src/facts.js
@@ -7,6 +7,13 @@ module.exports = {
         "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
         "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
         "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
-        "Replace me and add some more facts!"
+        "In 1952, Arthur Samuel develops the first computer checkers-playing program and the first computer program to learn on its own.",
+        "In 1957, Frank Rosenblatt develops the Perceptron, an early artificial neural network enabling pattern recognition based on a two-layer computer learning network. ",
+        "In 1958, John McCarthy develops programming language Lisp which becomes the most popular programming language used in artificial intelligence research.",
+        "The first industrial robot, Unimate, starts working on an assembly line in 1961 at a General Motors plant in New Jersey.",
+        "In 1986, David Rumelhart, Geoffrey Hinton, and Ronald Williams publish ”Learning representations by back-propagating errors,” in which they describe “a new learning procedure, back-propagation, for networks of neurone-like units.",
+        "In 2000, Honda's ASIMO robot, an artificially intelligent humanoid robot, is able to walk as fast as a human, delivering trays to customers in a restaurant setting.",
+        "Fei Fei Li and colleagues at Princeton University in 2007 started to assemble ImageNet, a large database of annotated images designed to aid in visual object recognition software research.",
+        "In 2011, Watson, a natural language question answering computer, competes on Jeopardy! and defeats two former champions."
     ]
 }
